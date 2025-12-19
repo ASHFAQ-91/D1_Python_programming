c839306372331a5122bc32ba9dcbc15572cbc7cc
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-20 01:19:34 +0400

    Add Initial Files

---
+++ Harry_1_V1to15_Concept/1Hello_World.py
@@ -0,0 +1,16 @@
+"""PYTHON LANGUAGES HELP IN THINGS:
+    Web Development.
+    Backend.
+    GUI Development.
+    Testing.
+    Hacking.
+    Data Analysis.
+    Future Helpin things also-> ML & Data Science."""
+print("Hello World")
+a = "'hello Cutie'"
+print(a);
+a = "''hello speed''"
+print(a);
+# a = ""hello hi""       //BUG: give error
+a = """hello Valo"""
+print(a);
