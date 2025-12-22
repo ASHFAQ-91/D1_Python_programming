2e96d397448b58307acc12dc862b9ee8dde82082
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-22 23:57:33 +0400

    4type

---
+++ Harry_1_V1to15_Concept/4TypeCasting.py
@@ -0,0 +1,13 @@
+# Type casting & Concate the string.
+var1 = "10"
+var2 = "5"
+
+print(var1 + var2 + "\n")   # because both variables are string so concate each other
+
+print(int(var1) + int(var2))    # I convert String to Integer (in my opinionExplicit TYPE CASTING)
+"""
+THREE TYPES OF DATATYPES IN PYTHON
+str()
+int()
+float()
+"""
