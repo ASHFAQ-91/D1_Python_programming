1071ab669edae38d4d75d65e944a4f75575a8702
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-22 23:47:31 +0400

    4TypeCasting.py

---
+++ Harry_1_V1to15_Concept/4ArithmeticOptr.py
@@ -0,0 +1,10 @@
+# Types of Operator:-
+# 1. Arithmetic Optr -> add, subtract, multi, division, floar division, modules.
+
+print(9+3)
+print(9-3)
+print(9*3)
+print(9/3)
+print(9//3)     # remove after point values.
+print(9%3)
+print(5**2)     # Exponential or we can say "Square".
