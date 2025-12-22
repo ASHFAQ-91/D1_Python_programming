5bb07eca976bd88edce0eac36b55878ff3dcd9f9
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-22 23:33:58 +0400

    2comment

---
+++ Harry_1_V1to15_Concept/2Commet.py
@@ -0,0 +1,20 @@
+# Single Line, multiple line comments, Escape Sequence Character as well as how to print String.
+
+# for Single line comment use Hashtag in the beginning.
+"""
+for
+Multiple
+Line Comment
+write text btw the triple single or double Quote.
+"""
+
+# Escape Sequence Character like - \n, \t, \", sep="", end=""
+# START:
+print("Hello, \nMy name is \"Ashfaq\"")
+print("Hey", 6, 7, sep="--||--")
+
+print("St. Xavier", end="'s ")
+print("College, Jaipur")
+# END:
+
+print("I'm pursuing BCA from", "St. Xavier's College")
