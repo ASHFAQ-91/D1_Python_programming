47cd9b63a59110569cbd3c5c6b1af9f4526cc66e
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-23 23:51:02 +0400

    4TypeCasting.py

--- Harry_1_V1to15_Concept/4TypeCasting.py
+++ Harry_1_V1to15_Concept/4TypeCasting.py
@@ -1,13 +1,16 @@
-# Type casting & Concate the string.
+# Type casting - Implicit & Explicit Type Casting.
 var1 = "10"
 var2 = "5"
 
 print(var1 + var2 + "\n")   # because both variables are string so concate each other
 
-print(int(var1) + int(var2))    # I convert String to Integer (in my opinionExplicit TYPE CASTING)
+print(int(var1) + int(var2))    # Str to Int (Explicit TYPE CASTING)
+
 """
-THREE TYPES OF DATATYPES IN PYTHON
-str()
-int()
-float()
+TYPES OF Explicit Type conversion IN PYTHON
+str(), int(), float(), ord(), hex(), oct(), list(), tuple(), dict(), set()
 """
+
+varX = 2        # int DataType
+varY = 5.5      # float DataType
+print(varX + varY)              # outcome in float (Implicit TYPE CASTING)
