11ff1644ce7f78926420f94adb0f4d2bb5573521
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-23 23:51:22 +0400

    5Multiple_value.py

---
+++ Harry_1_V1to15_Concept/5Multiple_value.py
@@ -0,0 +1,9 @@
+# print value at a multiple times.
+var1 = "10"
+var2 = "5"
+
+print(10 * "Hello World\n")         # multi with string
+
+print(10 * int(var1) + int(var2))   # multi with int
+
+print(10 * str(int(var1) + int(var2)))
