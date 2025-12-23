209e2a40bb8cbe5b1a69c804d18c34f03cf5173f
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-23 23:51:41 +0400

    6TakingUserInput.py

---
+++ Harry_1_V1to15_Concept/6TakingUserInput.py
@@ -0,0 +1,12 @@
+# Taking User Input.
+name = input("Enter your name: ")
+print("My name is", name)
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("Sum is: ", x+y)
+
+
+"""When we taking the value from the user, by default value comes with string datatype"""
+"""so when want to and 2 varibles, first we need to convert integer variable using Type-Casting"""
