8591316241e3a4bda7f03f432720bd405748bcc1
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-24 23:26:34 +0400

    9ConditionalStatement.py

---
+++ Harry_1_V1to15_Concept/9ConditionalStatement.py
@@ -0,0 +1,13 @@
+# Conditional Statement.
+# 1. if
+# 2. if-else
+# 3. if-else-elif
+# 4. nested if-else-elif
+
+print("*****Car Eligibility*****".center(40))
+age = int(input("Enter your Age: "))
+
+if(age>17):
+    print("You can Drive.")
+else:
+    print("You can't Drive.")
