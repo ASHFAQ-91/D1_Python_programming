e8a04bf4c79c111ead6eed63e04bb0b913787279
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2025-12-24 23:30:46 +0400

    11ForLoop.py

---
+++ Harry_1_V1to15_Concept/11ForLoop.py
@@ -0,0 +1,18 @@
+# Types of Iteration/Loop.
+# 1. For Loop.
+# 2. While Loop.
+
+#$ for string.
+# name = 'Ashfaq'
+# for i in name:
+#     print(i);
+
+#$ for List.
+colors = ['Red', 'Green', 'Blue']
+for i in colors:
+    print(i)
+
+for i in colors:
+    print("\n")
+    for j in i:
+        print(j)
